7a92788d9585f229d2efdc2331492c06ba5c819a
EshanDev <theerapong.b@ru.ac.th>
2020-08-18 13:27:34 +0700

    Change Web Route

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('home', function(){
     return view('welcome');
-});
-
-Route::get('/home', function(){
-    return 'Home';
 });
